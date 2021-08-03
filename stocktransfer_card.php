1af725f78b1c24e99e73056ba4939df7a034a5c7
Gauthier PC portable 024 <gauthier.verdol@atm-consulting.fr>
2021-08-03 16:45:10 +0200

    FIX : tms current timestamp on update

--- stocktransfer_card.php
+++ stocktransfer_card.php
@@ -726,6 +726,8 @@ if ($object->id > 0 && (empty($action) || ($action != 'edit' && $action != 'crea
 
 	$param = '';
 
+	$conf->global->MAIN_DISABLE_WRAPPING_ON_COLUMN_TITLE=true; // Full display needed to see all column title details
+
 	print '<tr class="liste_titre">';
 	print getTitleFieldOfList($langs->trans('ProductRef'), 0, $_SERVER["PHP_SELF"], '', $param, '', '', $sortfield, $sortorder, 'tagtd maxwidthonsmartphone ');
 	if ($conf->productbatch->enabled) {
